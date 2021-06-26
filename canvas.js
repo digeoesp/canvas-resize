fc07ae51df2f9dee63e37f2268ca84be02755d9e
digeoesp <diegocrypto@outlook.com>
2021-06-26 05:16:55 -0700

    created some boxes

--- canvas.js
+++ canvas.js
@@ -1,3 +1,8 @@
 let canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+let c = canvas.getContext('2d');
+c.fillRect(100, 100, 300, 300);
+c.fillRect(200, 200, 300, 300);
+c.fillRect(300, 300, 300, 300);

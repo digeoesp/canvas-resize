5938b00f99c1217e656daa3b05f69e8d7d743bac
digeoesp <diegocrypto@outlook.com>
2021-07-06 00:22:09 -0700

    added event listener

--- canvas.js
+++ canvas.js
@@ -44,7 +44,7 @@ for (let i = 0; i < 1000; i++) {
     let y = Math.random() * window.innerHeight;
     c.beginPath();
     c.arc(x, y, 10, 0, Math.PI * 2, false);
-    c.strokeStyle = getRandomColour();
+    c.strokeStyle = 'black';
     c.stroke();
 
 }
@@ -56,20 +56,43 @@ for (let i = 0; i < 1000; i++) {
 // let dx = (Math.random() - 0.5) * 30;
 // let dy = (Math.random() - 0.5) * 30;
 // let radius = 30;
+let mouse = {
+    x: undefined,
+    y: undefined
+}
+let maxRadius = 40;
+let minRadius = 2;
+
+window.addEventListener('mousemove', function (e) {
+    mouse.x = e.x;
+    mouse.y = e.y;
+    console.log(mouse)
+})
+let colorArray = [
+    '#ffaa33',
+    '#99ffaaa',
+    '#00ff00',
+    '#4411aa',
+    '#ff1100',
+]
+
 function Circle(x, y, dx, dy, radius) {
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
     this.radius = radius
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
     this.draw = function () {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-        c.strokeStyle = getRandomColour();
-        c.stroke();
+        c.fillStyle = this.color;
+        c.fill();
+
 
     }
+
     this.update = function () {
         if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
             this.dx = -this.dx;
@@ -82,13 +105,24 @@ function Circle(x, y, dx, dy, radius) {
 
         this.x += this.dx;
         this.y += this.dy;
+        //interactivity
+        if (mouse.x - this.x < 50 && mouse.x - this.x > -50
+            && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+            if (this.radius < maxRadius) {
+                this.radius += 1;
+            }
+        } else if (this.radius > minRadius) {
+            this.radius -= 1;
+        }
+
+
         this.draw();
 
     }
 }
 let circleArray = [];
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 1000; i++) {
     let radius = 30;
     let x = Math.random() * (innerWidth - radius * 2) + radius;
     let y = Math.random() * (innerHeight - radius * 2) + radius;
